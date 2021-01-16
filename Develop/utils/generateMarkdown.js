bacab3a7f231e42ad254f05cb5f237f8abb014e4
Nilda <nick.ovalles@gmail.com>
2021-01-15 19:26:11 -0700

    <code dump>

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,50 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// create const for badges
+const badges = {
+  "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+  "Apache": "[![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+  "Mozilla": "[![License: Mozilla](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+  "Unlicense": "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"
+};
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// create const for license sites
+const licenseLinks = {
+  "MIT": "https://opensource.org/licenses/MIT",
+  "Apache": "https://opensource.org/licenses/Apache-2.0",
+  "Mozilla": "https://opensource.org/licenses/MPL-2.0",
+  "Unlicense": "http://unlicense.org/"
+};
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${badges[data.license]}
+  ## Description
+  ${data.describe}
+  ## Table of Contents
+  
+  * [Title](#title)
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## Credits
+  ${data.credits}
+  ## License
+  License is covered under: ${licenseLinks[data.license]}
+  ## Contributing
+  ${data.contrib}
+  ## Tests
+  ${data.tests}
+  ## Questions
+  If you have any questions, you can reach me by emailing [${data.email}](mailto:${data.email}) or follow the link to my gitHub profile [${data.userName}](https://github.com/${data.userName}).
 `;
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # Professional README Generator Starter Code
 
-[How to create a Professional README](./readme-guide.md)
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  Here is a link that demonstrates the functionality of the README generator![Link](#)
+ Follow this link that demonstrates the functionality of the README generator! [README Generator Walkthrough Video](#)
+
+
+  ## Description
+  Generate a professional README file simply by answering a few questions about your application.
+  ## Table of Contents
+  
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  Clone the repo, create new repo, and copy cloned repo into your new directory.
+  ## Usage
+  Launch terminal, run command node index, answer the questions as prompted and your README file will generate.
+  ## Credits
+  No other collaborators at this time.
+  ## License
+  License is covered under: https://opensource.org/licenses/MIT
+  ## Contributing
+  Not at this time.
+  ## Tests
+  There are no tests at this time.
+  ## Questions
+  If you have any questions, you can reach me by emailing [nick.ovalles@gmail.com](mailto:nick.ovalles@gmail.com) or follow the link to my gitHub profile [nickovalles](https://github.com/nickovalles).
